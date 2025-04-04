237d4308dca1d3046c1cd310e4403e38ee5e3122
Кирилл Майоров <k.mayorov@megaplan.ru>
2025-04-04 20:59:02 +0300

    Python Advanced: Домашнее задание 9

--- hw8/admin.py
+++ hw8/admin.py
@@ -2,6 +2,26 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Task)
-admin.site.register(SubTask)
-admin.site.register(Category)
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'status', 'deadline', 'created_at')
+    search_fields = ('title', 'description')
+    list_filter = ('categories', 'deadline')
+    ordering = ('-deadline', 'title')
+    list_per_page = 20
+
+class SubTaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'task', 'description', 'status', 'deadline', 'created_at')
+    search_fields = ('title', 'description')
+    list_filter = ('task', 'deadline')
+    ordering = ('-deadline', 'title')
+    list_per_page = 20
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_per_page = 20
+
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(SubTask, SubTaskAdmin)
+admin.site.register(Category, CategoryAdmin)
