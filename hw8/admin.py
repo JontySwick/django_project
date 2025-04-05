27fa7eeb99c1b93897485c21e5800dcb60588b58
Кирилл Майоров <k.mayorov@megaplan.ru>
2025-04-05 10:12:02 +0300

    Python Advanced: Домашнее задание 11

--- hw8/admin.py
+++ hw8/admin.py
@@ -3,12 +3,25 @@ from django.contrib import admin
 from .models import *
 
 
+class SubTaskInline(admin.StackedInline):
+    model = SubTask
+    extra = 1
+
+
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'status', 'deadline', 'created_at')
+    def short_title(self, obj):
+        if len(obj.title) > 13:
+            return f'{obj.title[:10]}...'
+
+        return obj.title
+
+    list_display = ('short_title', 'description', 'status', 'deadline', 'created_at')
     search_fields = ('title', 'description')
     list_filter = ('categories', 'deadline')
     ordering = ('-deadline', 'title')
     list_per_page = 20
+    inlines = [SubTaskInline]
+
 
 class SubTaskAdmin(admin.ModelAdmin):
     list_display = ('title', 'task', 'description', 'status', 'deadline', 'created_at')
@@ -16,6 +29,13 @@ class SubTaskAdmin(admin.ModelAdmin):
     list_filter = ('task', 'deadline')
     ordering = ('-deadline', 'title')
     list_per_page = 20
+
+    def action_filter_done(self, request, queryset):
+        queryset.filter(status__iexact="Done")
+
+    action_filter_done.short_description = "Filter objects with status is Done"
+
+    actions = [action_filter_done]
 
 
 class CategoryAdmin(admin.ModelAdmin):
